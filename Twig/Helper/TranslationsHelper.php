f2c97638c08e9b3a818800f84124d8a5fffa05d6
Alexander Murashov <rodger@ladela.com>
2014-05-12 14:42:20 +0300

    refctoring translations helper

--- Twig/Helper/TranslationsHelper.php
+++ Twig/Helper/TranslationsHelper.php
@@ -2,64 +2,86 @@
 
 namespace Ladela\PersonalTranslationsWidgetBundle\Twig\Helper;
 
+use Doctrine\ORM\EntityManager;
 use Gedmo\Translatable\TranslatableListener;
-use Doctrine\Bundle\DoctrineBundle\Registry;
-use Symfony\Component\DependencyInjection\Container;
 use Ladela\PersonalTranslationsWidgetBundle\TranslationsGetter\TranslationsGetterInterface;
+use Symfony\Component\DependencyInjection\ContainerAware;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
-class TranslationsHelper extends \Symfony\Component\DependencyInjection\ContainerAware
+/**
+ * Class TranslationsHelper
+ *
+ * @package Ladela\PersonalTranslationsWidgetBundle\Twig\Helper
+ */
+class TranslationsHelper implements ContainerAwareInterface
 {
-  protected $gedmo_translatable_listener;
-  protected $doctrine;
-  protected $languages = array();
+    /** @var TranslatableListener */
+    protected $gedmo_translatable_listener;
+    /** @var EntityManager */
+    protected $em;
+    /** @var array */
+    protected $languages = array();
 
-  public function __construct(TranslatableListener $gedmo_translatable_listener, Registry $doctrine, Container $container)
-  {
-    $this->gedmo_translatable_listener = $gedmo_translatable_listener;
-    $this->doctrine                    = $doctrine;
-
-    $languages = $container->getParameter('ladela_personal_translations.languages');
-    $getter = $container->getParameter('ladela_personal_translations.getter');
-
-    if (!empty($languages))
+    /**
+     * Object constructor
+     *
+     * @param TranslatableListener $gedmo_translatable_listener
+     * @param EntityManager        $em
+     */
+    public function __construct(TranslatableListener $gedmo_translatable_listener, EntityManager $em)
     {
-      $this->languages = $languages;
+        $this->gedmo_translatable_listener = $gedmo_translatable_listener;
+        $this->em                          = $em;
     }
-    elseif(!empty($getter))
+
+    /**
+     * Sets container
+     *
+     * @param ContainerInterface $container
+     */
+    public function setContainer(ContainerInterface $container = null)
     {
-      $getter = $container->get($getter);
-      if ($getter instanceof TranslationsGetterInterface)
-      {
-        $this->languages = $getter->getLanguages();
-      }
+        $languages = $container->getParameter('ladela_personal_translations.languages');
+        $getter    = $container->getParameter('ladela_personal_translations.getter');
+
+        if (!empty($languages)) {
+            $this->languages = $languages;
+        } elseif (!empty($getter)) {
+            $getter = $container->get($getter);
+            if ($getter instanceof TranslationsGetterInterface) {
+                $this->languages = $getter->getLanguages();
+            }
+        }
     }
-  }
 
-  /**
-   * Gets entity available fields
-   * @param $entity
-   * @return array
-   * @throws NoTranslationsDefinedException
-   */
-  public function getEntityTranslatableFields($entity)
-  {
-    $translatable_config = $this->gedmo_translatable_listener->getConfiguration(
-      $this->doctrine->getManager(),
-      get_class($entity)
-    );
+    /**
+     * Gets entity available fields
+     *
+     * @param $entity
+     *
+     * @return array
+     * @throws NoTranslationsDefinedException
+     */
+    public function getEntityTranslatableFields($entity)
+    {
+        $translatable_config = $this->gedmo_translatable_listener->getConfiguration($this->em, get_class($entity));
 
-    $fields = @$translatable_config['fields'];
-    if (!is_array($fields) || count($fields) == 0) throw new NoTranslationsDefinedException('No translations defined');
+        $fields = @$translatable_config['fields'];
+        if (!is_array($fields) || count($fields) == 0) {
+            throw new NoTranslationsDefinedException('No translations defined');
+        }
 
-    return $translatable_config['fields'];
-  }
+        return $translatable_config['fields'];
+    }
 
-  /**
-   * Gets available languages for translation
-   * @return array
-   */
-  public function getLanguages()
-  {
-    return $this->languages;
-  }
+    /**
+     * Gets available languages for translation
+     *
+     * @return array
+     */
+    public function getLanguages()
+    {
+        return $this->languages;
+    }
 }
